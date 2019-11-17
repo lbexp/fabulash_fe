d23159d8d7e3c97422f752836c58adea4770a5a6
Fiqar Aprialim <fiqarapr@gmail.com>
2019-11-18 00:15:21 +0800

    user therapist but croppie and img upload still not finished

---
+++ assets/js/script/datatable_therapist.js
@@ -0,0 +1,489 @@
+"use strict";
+
+// begin: Admin Page
+var KTDatatablesSearchOptionsAdvancedSearch = function() {
+
+    $.fn.dataTable.Api.register('column().title()', function() {
+        return $(this.header()).text().trim();
+    });
+
+    var initTableActiveTreatment = function() {
+        // begin first table
+        var table = $('#table_treatment_active').DataTable({
+            responsive: true,
+            // Pagination settings
+            dom: `<'row'<'col-sm-12'tr>><'row'<'col-sm-12 col-md-5'i><'col-sm-12 col-md-7 dataTables_pager'lp>>`,
+            // read more: https://datatables.net/examples/basic_init/dom.html
+            lengthMenu: [5, 10, 25, 50],
+            pageLength: 10,
+            language: {
+                'lengthMenu': 'Display _MENU_',
+            },
+            searchDelay: 500,
+            processing: true,
+            serverSide: false,
+            ajax: {
+                url: 'source/therapist/treatment.json',
+                type: 'POST',
+                data: {
+                    // parameters for custom backend script demo
+                    columnsDef: [
+                        'no', 'depot', 'vendor', 'pekerjaan', 'sifat',
+                        'tanggal', 'status', 'aksi',
+                    ],
+                },
+            },
+            columns: [{
+                data: 'null',
+                title: 'No',
+                render: function(data, type, row, meta) {
+                    return meta.row + meta.settings._iDisplayStart + 1;
+                },
+                // title: 'No.',
+                orderable: false,
+            }, {
+                data: 'tanggal',
+                title: 'Tanggal',
+            }, {
+                data: 'customer',
+                title: 'Customer',
+            }, {
+                data: 'status',
+                title: 'Status',
+                // width: 70,
+                render: function(data, type, full, meta) {
+                    var status = {
+                        scheduled: {
+                            'title': 'Scheduled',
+                            'class': 'btn-label-warning'
+                        },
+                        ongoing: {
+                            'title': 'Ongoing',
+                            'class': 'btn-label-primary'
+                        },
+                    };
+                    if (typeof status[data] === 'undefined') {
+                        return data;
+                    }
+                    return '<span style="width:100%" class="btn btn-bold btn-sm btn-font-sm ' + status[data].class + '">' + status[data].title + '</span>';
+                },
+            },{
+                field: 'aksi',
+                title: 'Aksi',
+                responsivePriority: -1,
+                className: 'text-center',
+                orderable: false,
+                width: 100,
+                render: function(data, type, full, meta) {
+                    var status = {
+                        scheduled: {'href': 'treatment_detail_scheduled.html'},
+                        ongoing : {'href': 'treatment_detail_ongoing.html'},
+                    };
+                    return `
+                    <a href="user_therapist/${status[full.status].href}" class="btn btn-sm btn-brand" style="color:white;border-radius:15px">Rincian</a>`;
+                },
+            }],
+            columnDefs: [{
+                targets: [0, 1, 2, 3, 4],
+                className: 'text-center',
+                orderable: true,
+            }],
+        });
+
+        table.on('order.dt search.dt', function() {
+            table.column(0, {
+                search: 'applied',
+                order: 'applied'
+            }).nodes().each(function(cell, i) {
+                cell.innerHTML = i + 1;
+            });
+        }).draw();
+
+        var filter = function() {
+            var val = $.fn.dataTable.util.escapeRegex($(this).val());
+            table.column($(this).data('col-index')).search(val ? val : '', false, false).draw();
+        };
+
+        var asdasd = function(value, index) {
+            var val = $.fn.dataTable.util.escapeRegex(value);
+            table.column(index).search(val ? val : '', false, true);
+        };
+
+        $('#kt_search_waktu').on('change', function(e) {
+            e.preventDefault();
+            var params = {};
+            $('.kt-input').each(function() {
+                var i = $(this).data('col-index');
+                if (params[i]) {
+                    params[i] += '|' + $(this).val();
+                } else {
+                    params[i] = $(this).val();
+                }
+            });
+            $.each(params, function(i, val) {
+                // apply search params to datatable
+                table.column(i).search(val ? val : '', false, false);
+            });
+            table.table().draw();
+        });
+
+        $('#kt_search_all').on('keyup', function() {
+            table.search(this.value).draw();
+        });
+
+        $.fn.datepicker.dates['id'] = {
+            days: 'Minggu_Senin_Selasa_Rabu_Kamis_Jumat_Sabtu'.split('_'),
+            daysShort: 'Min_Sen_Sel_Rab_Kam_Jum_Sab'.split('_'),
+            daysMin: 'Mg_Sn_Sl_Rb_Km_Jm_Sb'.split('_'),
+            months: 'Januari_Februari_Maret_April_Mei_Juni_Juli_Agustus_September_Oktober_November_Desember'.split('_'),
+            monthsShort: 'Jan_Feb_Mar_Apr_Mei_Jun_Jul_Agt_Sep_Okt_Nov_Des'.split('_'),
+            today: "Hari ini",
+            clear: "Reset",
+            format: "dd MM yyyy",
+            titleFormat: "MM yyyy"
+        };
+
+        $('#kt_search_waktu').datepicker({
+            todayHighlight: true,
+            language: 'id',
+            rtl: KTUtil.isRTL(),
+            todayBtn: "linked",
+            clearBtn: true,
+            templates: {
+                leftArrow: '<i class="la la-angle-left"></i>',
+                rightArrow: '<i class="la la-angle-right"></i>',
+            },
+        })
+        // Set current date
+        // .datepicker("setDate", new Date());
+    };
+
+    var initTableListTreatment = function() {
+        // begin first table
+        var table = $('#table_treatment_list').DataTable({
+            responsive: true,
+            // Pagination settings
+            dom: `<'row'<'col-sm-12'tr>><'row'<'col-sm-12 col-md-5'i><'col-sm-12 col-md-7 dataTables_pager'lp>>`,
+            // read more: https://datatables.net/examples/basic_init/dom.html
+            lengthMenu: [5, 10, 25, 50],
+            pageLength: 10,
+            language: {
+                'lengthMenu': 'Display _MENU_',
+            },
+            searchDelay: 500,
+            processing: true,
+            serverSide: false,
+            ajax: {
+                url: 'source/therapist/treatment.json',
+                type: 'POST',
+                data: {
+                    // parameters for custom backend script demo
+                    columnsDef: [
+                        'no', 'depot', 'vendor', 'pekerjaan', 'sifat',
+                        'tanggal', 'status', 'aksi',
+                    ],
+                },
+            },
+            columns: [{
+                data: 'null',
+                title: 'No',
+                render: function(data, type, row, meta) {
+                    return meta.row + meta.settings._iDisplayStart + 1;
+                },
+                // title: 'No.',
+                orderable: false,
+            }, {
+                data: 'tanggal',
+                title: 'Tanggal',
+            }, {
+                data: 'no_pesanan',
+                title: 'No. Pesanan',
+            }, {
+                data: 'treatment',
+                title: 'Treatment',
+            }, {
+                data: 'customer',
+                title: 'Customer',
+            }, {
+                data: 'durasi',
+                title: 'Durasi',
+            },{
+                field: 'aksi',
+                title: 'Aksi',
+                responsivePriority: -1,
+                className: 'text-center',
+                orderable: false,
+                width: 100,
+                render: function(data, type, full, meta) {
+                    return `
+                    <a href="user_therapist/treatment_detail_paid.html" class="btn btn-sm btn-brand" style="color:white;border-radius:15px">Rincian</a>`;
+                },
+            }],
+            columnDefs: [{
+                targets: [0, 1, 2, 3, 4, 5, 6],
+                className: 'text-center',
+                orderable: true,
+            }],
+        });
+
+        table.on('order.dt search.dt', function() {
+            table.column(0, {
+                search: 'applied',
+                order: 'applied'
+            }).nodes().each(function(cell, i) {
+                cell.innerHTML = i + 1;
+            });
+        }).draw();
+
+        var filter = function() {
+            var val = $.fn.dataTable.util.escapeRegex($(this).val());
+            table.column($(this).data('col-index')).search(val ? val : '', false, false).draw();
+        };
+
+        var asdasd = function(value, index) {
+            var val = $.fn.dataTable.util.escapeRegex(value);
+            table.column(index).search(val ? val : '', false, true);
+        };
+
+        $('#kt_search_waktu').on('change', function(e) {
+            e.preventDefault();
+            var params = {};
+            $('.kt-input').each(function() {
+                var i = $(this).data('col-index');
+                if (params[i]) {
+                    params[i] += '|' + $(this).val();
+                } else {
+                    params[i] = $(this).val();
+                }
+            });
+            $.each(params, function(i, val) {
+                // apply search params to datatable
+                table.column(i).search(val ? val : '', false, false);
+            });
+            table.table().draw();
+        });
+
+        $('#kt_search_all').on('keyup', function() {
+            table.search(this.value).draw();
+        });
+
+        $.fn.datepicker.dates['id'] = {
+            days: 'Minggu_Senin_Selasa_Rabu_Kamis_Jumat_Sabtu'.split('_'),
+            daysShort: 'Min_Sen_Sel_Rab_Kam_Jum_Sab'.split('_'),
+            daysMin: 'Mg_Sn_Sl_Rb_Km_Jm_Sb'.split('_'),
+            months: 'Januari_Februari_Maret_April_Mei_Juni_Juli_Agustus_September_Oktober_November_Desember'.split('_'),
+            monthsShort: 'Jan_Feb_Mar_Apr_Mei_Jun_Jul_Agt_Sep_Okt_Nov_Des'.split('_'),
+            today: "Hari ini",
+            clear: "Reset",
+            format: "dd MM yyyy",
+            titleFormat: "MM yyyy"
+        };
+
+        $('#kt_search_waktu').datepicker({
+            todayHighlight: true,
+            language: 'id',
+            rtl: KTUtil.isRTL(),
+            todayBtn: "linked",
+            clearBtn: true,
+            templates: {
+                leftArrow: '<i class="la la-angle-left"></i>',
+                rightArrow: '<i class="la la-angle-right"></i>',
+            },
+        })
+        // Set current date
+        // .datepicker("setDate", new Date());
+    };
+
+    var initTableComplaint = function() {
+        // begin first table
+        var table = $('#table_complaint').DataTable({
+            responsive: true,
+            // Pagination settings
+            dom: `<'row'<'col-sm-12'tr>><'row'<'col-sm-12 col-md-5'i><'col-sm-12 col-md-7 dataTables_pager'lp>>`,
+            // read more: https://datatables.net/examples/basic_init/dom.html
+            lengthMenu: [5, 10, 25, 50],
+            pageLength: 10,
+            language: {
+                'lengthMenu': 'Display _MENU_',
+            },
+            searchDelay: 500,
+            processing: true,
+            serverSide: false,
+            ajax: {
+                url: 'source/therapist/treatment.json',
+                type: 'POST',
+                data: {
+                    // parameters for custom backend script demo
+                    columnsDef: [
+                        'no', 'depot', 'vendor', 'pekerjaan', 'sifat',
+                        'tanggal', 'status', 'aksi',
+                    ],
+                },
+            },
+            columns: [{
+                data: 'null',
+                title: 'No',
+                render: function(data, type, row, meta) {
+                    return meta.row + meta.settings._iDisplayStart + 1;
+                },
+                // title: 'No.',
+                orderable: false,
+            }, {
+                data: 'tanggal',
+                title: 'Tanggal',
+            }, {
+                data: 'no_pesanan',
+                title: 'No. Pesanan',
+            }, {
+                data: 'treatment',
+                title: 'Treatment',
+            }, {
+                data: 'customer',
+                title: 'Customer',
+            }, {
+                data: 'complaint',
+                title: 'Complaint',
+            },{
+                field: 'aksi',
+                title: 'Aksi',
+                responsivePriority: -1,
+                className: 'text-center',
+                orderable: false,
+                width: 100,
+                render: function(data, type, full, meta) {
+                    return `
+                    <a href="user_therapist/treatment_detail_paid.html" class="btn btn-sm btn-brand" style="color:white;border-radius:15px">Rincian</a>`;
+                },
+            }],
+            columnDefs: [{
+                targets: [0, 1, 2, 3, 4, 5, 6],
+                className: 'text-center',
+                orderable: true,
+            }],
+        });
+
+        table.on('order.dt search.dt', function() {
+            table.column(0, {
+                search: 'applied',
+                order: 'applied'
+            }).nodes().each(function(cell, i) {
+                cell.innerHTML = i + 1;
+            });
+        }).draw();
+
+        var filter = function() {
+            var val = $.fn.dataTable.util.escapeRegex($(this).val());
+            table.column($(this).data('col-index')).search(val ? val : '', false, false).draw();
+        };
+
+        var asdasd = function(value, index) {
+            var val = $.fn.dataTable.util.escapeRegex(value);
+            table.column(index).search(val ? val : '', false, true);
+        };
+
+        $('#kt_search_waktu').on('change', function(e) {
+            e.preventDefault();
+            var params = {};
+            $('.kt-input').each(function() {
+                var i = $(this).data('col-index');
+                if (params[i]) {
+                    params[i] += '|' + $(this).val();
+                } else {
+                    params[i] = $(this).val();
+                }
+            });
+            $.each(params, function(i, val) {
+                // apply search params to datatable
+                table.column(i).search(val ? val : '', false, false);
+            });
+            table.table().draw();
+        });
+
+        $('#kt_search_all').on('keyup', function() {
+            table.search(this.value).draw();
+        });
+
+        $.fn.datepicker.dates['id'] = {
+            days: 'Minggu_Senin_Selasa_Rabu_Kamis_Jumat_Sabtu'.split('_'),
+            daysShort: 'Min_Sen_Sel_Rab_Kam_Jum_Sab'.split('_'),
+            daysMin: 'Mg_Sn_Sl_Rb_Km_Jm_Sb'.split('_'),
+            months: 'Januari_Februari_Maret_April_Mei_Juni_Juli_Agustus_September_Oktober_November_Desember'.split('_'),
+            monthsShort: 'Jan_Feb_Mar_Apr_Mei_Jun_Jul_Agt_Sep_Okt_Nov_Des'.split('_'),
+            today: "Hari ini",
+            clear: "Reset",
+            format: "dd MM yyyy",
+            titleFormat: "MM yyyy"
+        };
+
+        $('#kt_search_waktu').datepicker({
+            todayHighlight: true,
+            language: 'id',
+            rtl: KTUtil.isRTL(),
+            todayBtn: "linked",
+            clearBtn: true,
+            templates: {
+                leftArrow: '<i class="la la-angle-left"></i>',
+                rightArrow: '<i class="la la-angle-right"></i>',
+            },
+        })
+        // Set current date
+        // .datepicker("setDate", new Date());
+    };
+
+    var initTableTreatmentDetail = function() {
+        var table = $('#table_treatment_detail');
+        // begin first table
+        table.DataTable({
+            info: false,
+            paging: false,
+            lengthChange: false,
+            searching: false,
+            responsive: true,
+            ajax: {
+                url: 'source/treatment_list.json',
+                type: 'POST',
+                data: {
+                    pagination: {
+                        perpage: 50,
+                    },
+                },
+            },
+            columns: [{
+                data: 'null',
+                title: 'No',
+                render: function(data, type, row, meta) {
+                    return meta.row + meta.settings._iDisplayStart + 1;
+                },
+                // title: 'No.',
+                orderable: false,
+            }, {
+                data: 'treatment',
+                title: 'Treatment'
+            }, {
+                data: 'harga',
+                title: 'Harga'
+            }, ],
+            columnDefs: [{
+                targets: [0, 1, 2],
+                className: 'text-center',
+                orderable: true,
+            }],
+        });
+    };
+
+    return {
+        //main function to initiate the module
+        init: function() {
+            initTableActiveTreatment();
+            initTableListTreatment();
+            initTableComplaint();
+            initTableTreatmentDetail();
+        },
+    };
+
+}();
+
+
+jQuery(document).ready(function() {
+    KTDatatablesSearchOptionsAdvancedSearch.init();
+});
