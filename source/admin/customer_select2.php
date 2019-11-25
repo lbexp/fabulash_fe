ae8df4649ece8a8537bab9ae41b939ca93f58fc6
Fiqar Aprialim <fiqarapr@gmail.com>
2019-11-25 11:59:42 +0800

    Select2 jadi, no bloodhound and nohp yet

---
+++ source/admin/customer_select2.php
@@ -0,0 +1,23 @@
+<?php
+
+define (DB_USER, "root");
+define (DB_PASSWORD, "585259");
+define (DB_DATABASE, "customer");
+define (DB_HOST, "localhost");
+$mysqli = new mysqli(DB_HOST, DB_USER, DB_PASSWORD, DB_DATABASE);
+
+
+$sql = "SELECT customer.id, customer.nama FROM customer
+		WHERE nama LIKE '%".$_GET['q']."%'
+		LIMIT 10";
+$result = $mysqli->query($sql);
+
+
+$json = [];
+while($row = $result->fetch_assoc()){
+     $json[] = ['id'=>$row['id'], 'text'=>$row['nama']];
+}
+
+echo json_encode($json);
+
+?>
