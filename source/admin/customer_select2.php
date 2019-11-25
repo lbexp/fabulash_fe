5b649e7b51c6123b33f77ed6c28ead3a97ef8f09
Fiqar Aprialim <fiqarapr@gmail.com>
2019-11-25 15:24:09 +0800

    Select2 with no_hp but still in the same field

--- source/admin/customer_select2.php
+++ source/admin/customer_select2.php
@@ -7,7 +7,7 @@ define (DB_HOST, "localhost");
 $mysqli = new mysqli(DB_HOST, DB_USER, DB_PASSWORD, DB_DATABASE);
 
 
-$sql = "SELECT customer.id, customer.nama FROM customer
+$sql = "SELECT customer.id, customer.nama, customer.no_hp FROM customer
 		WHERE nama LIKE '%".$_GET['q']."%'
 		LIMIT 10";
 $result = $mysqli->query($sql);
@@ -15,7 +15,7 @@ $result = $mysqli->query($sql);
 
 $json = [];
 while($row = $result->fetch_assoc()){
-     $json[] = ['id'=>$row['id'], 'text'=>$row['nama']];
+     $json[] = ['id'=>$row['id'], 'text'=>$row['nama'], 'slug'=>$row['no_hp']];
 }
 
 echo json_encode($json);
