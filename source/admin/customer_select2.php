143fba0098acc07323ff98fde4efadacf6f44bb4
Fiqar Aprialim <fiqarapr@gmail.com>
2019-11-26 22:37:28 +0800

    Tambah tombol form treatment active & scheduled, perubahan tampilan form, pencocokan id dgn kontennya, catatan form dihilangkan, nohp = id utama customer

--- source/admin/customer_select2.php
+++ source/admin/customer_select2.php
@@ -8,14 +8,14 @@ $mysqli = new mysqli(DB_HOST, DB_USER, DB_PASSWORD, DB_DATABASE);
 
 
 $sql = "SELECT customer.id, customer.nama, customer.no_hp FROM customer
-		WHERE nama LIKE '%".$_GET['q']."%'
+		WHERE no_hp LIKE '%".$_GET['q']."%'
 		LIMIT 10";
 $result = $mysqli->query($sql);
 
 
 $json = [];
 while($row = $result->fetch_assoc()){
-     $json[] = ['id'=>$row['id'], 'text'=>$row['nama'], 'number'=>$row['no_hp']];
+     $json[] = ['id'=>$row['id'], 'nama'=>$row['nama'], 'number'=>$row['no_hp']];
 }
 
 echo json_encode($json);
